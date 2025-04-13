b171d440614a48b8c20916d52450cd3767835794
hexi0721 <68277623+hexi0721@users.noreply.github.com>
2025-04-14 00:31:45 +0800

    4_14-1

--- Assets/Scenes/Quest Arrow Pointer/QAP_GameHandler.cs
+++ Assets/Scenes/Quest Arrow Pointer/QAP_GameHandler.cs
@@ -1,13 +1,56 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+
 
 public class QAP_GameHandler : MonoBehaviour
 {
-
+    [SerializeField] List<Vector3> targetPositionList;
     [SerializeField] QAP_WindowQuestPointer windowQuestPointer;
+
+    [SerializeField] List<Color> color;
+
+    [SerializeField] Transform 玩家;
 
     private void Start()
     {
-        windowQuestPointer.Show(new Vector3(5 , 4));
+
+        int count = Random.Range(3, 8);
+
+        for (int i = 0; i < count; i++)
+        {
+            targetPositionList.Add(new Vector3(Random.Range(-15f, 15f), Random.Range(-9f, 9f)));
+        }
+
+        windowQuestPointer.CreatePointer(玩家 , targetPositionList[^1], Random.Range(0, 1), color[1]);
+        windowQuestPointer.CreatePointer(玩家 , targetPositionList[^2], Random.Range(0, 1), color[2]);
+
+
+        foreach (Vector3 targetPosition in targetPositionList)
+        {
+            if(targetPosition == targetPositionList[^1] || targetPosition == targetPositionList[^2])
+            {
+                break;
+            }
+
+            windowQuestPointer.CreatePointer(玩家 , targetPosition, Random.Range(0, 1), color[0]);
+        }
+
+
+    }
+
+    private void Update()
+    {
+        
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        if(v != 0 || h != 0)
+        {
+            玩家.position += new Vector3(h, v) * Time.deltaTime * 5f;
+        }
+
     }
 
 }
--- Assets/Scenes/Quest Arrow Pointer/QAP_WindowQuestPointer.cs
+++ Assets/Scenes/Quest Arrow Pointer/QAP_WindowQuestPointer.cs
@@ -1,81 +1,186 @@
+using NUnit.Framework;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class QAP_WindowQuestPointer : MonoBehaviour
 {
 
     [SerializeField] Camera uiCam;
-    [SerializeField] Vector3 targetPosition;
-    [SerializeField] Sprite spritePointer , spriteQuestion;
+    
+    [SerializeField] Sprite spritePointer;
+    [SerializeField] Sprite spriteQuestion;
+    [SerializeField] Sprite spriteAnswer;
 
-    Image image;
-    RectTransform pointer;
+    private List<QuestPointer> questPointerList;
 
-    private void Awake()
+
+    private void Start()
     {
-        pointer = transform.Find("Pointer").GetComponent<RectTransform>();
-        image = pointer.GetComponent<Image>();
-
-        Hide();
+        questPointerList = new List<QuestPointer>();
     }
 
     private void Update()
     {
 
-        Vector3 targetScreenPosition = uiCam.WorldToScreenPoint(targetPosition);
-        float borderSize = 40f;
-        bool isOffScreen = targetScreenPosition.x <= borderSize || targetScreenPosition.x >= Screen.width - borderSize || targetScreenPosition.y <= borderSize || targetScreenPosition.y >= Screen.height - borderSize;
-
-        Vector3 captureTargetScreenPosition = targetScreenPosition;
-        if (isOffScreen)
+        foreach (QuestPointer questPointer in new List<QuestPointer>(questPointerList))
         {
-            RotatePointerTowardsTarget();
-            
-            image.sprite = spritePointer;
-            
-            if (captureTargetScreenPosition.x <= borderSize) captureTargetScreenPosition.x = borderSize;
-            if (captureTargetScreenPosition.x >= Screen.width - borderSize) captureTargetScreenPosition.x = Screen.width - borderSize;
-            if (captureTargetScreenPosition.y <=  borderSize) captureTargetScreenPosition.y = borderSize;
-            if (captureTargetScreenPosition.y >= Screen.height - borderSize) captureTargetScreenPosition.y = Screen.height - borderSize;
-
-            //?? Vector3 pointerWorldPosition = uiCam.ScreenToWorldPoint(captureTargetScreenPosition);
-            pointer.position = captureTargetScreenPosition;
-            pointer.localPosition = new Vector3(pointer.localPosition.x, pointer.localPosition.y, 0f);
-        }
-        else
-        {
-            image.sprite = spriteQuestion;
-
-            pointer.position = captureTargetScreenPosition;
-            pointer.localPosition = new Vector3(pointer.localPosition.x, pointer.localPosition.y, 0f);
-            pointer.localEulerAngles = new Vector3(0, 0, 0);
+            if(questPointer.Update())
+            {
+                questPointerList.Remove(questPointer);
+            }
         }
 
-
-
     }
 
-    private void RotatePointerTowardsTarget()
+    public QuestPointer CreatePointer(Transform player ,  Vector3 targetPosition , int stat , Color color)
     {
-        Vector3 toPosition = targetPosition;
-        Vector3 fromPosition = uiCam.transform.position;
-        fromPosition.z = 0;
 
-        Vector3 dir = (toPosition - fromPosition).normalized;
-        float eulerZ = Utils.GetAngleFromVector(dir);
-        pointer.localEulerAngles = new Vector3(0, 0, eulerZ);
-    }
-
-    public void Hide()
-    {
-        gameObject.SetActive(false);
-    }
-
-    public void Show(Vector3 targetPosition)
-    {
+        Transform pointerDuplicate = Instantiate(transform.Find("PointerTemplate"), transform);
+        pointerDuplicate.gameObject.SetActive(true);
+        QuestPointer questPointer = new QuestPointer(player, targetPosition, pointerDuplicate, spritePointer, spriteQuestion , spriteAnswer, color , stat , uiCam);
         
-        gameObject.SetActive(true);
-        this.targetPosition = targetPosition;
+        questPointerList.Add(questPointer);
+        return questPointer;
+    }
+
+    public class QuestPointer
+    {
+        Transform player;
+        Vector3 targetPosition;
+        Transform transform;
+        Sprite spritePointer;
+        Sprite spriteInstruct;
+        
+        Sprite spriteAnswer;
+        
+        RectTransform rect;
+        Image image;
+        Color color;
+        int stat;
+        Camera uiCam;
+
+        public QuestPointer(Transform player ,  Vector3 targetPosition , Transform transform , Sprite spritePointer, Sprite spriteQuestion , Sprite spriteAnswer, Color color, int stat , Camera uiCam)
+        {
+            this.player = player;
+            this.targetPosition = targetPosition;
+            this.transform = transform;
+            this.spritePointer = spritePointer;
+            
+            this.spriteAnswer = spriteAnswer;
+
+            this.uiCam = uiCam;
+            this.stat = stat;
+
+            if(stat == 0)
+            {
+                spriteInstruct = spriteQuestion;
+            }
+            else if (stat == 1)
+            {
+                spriteInstruct = spriteAnswer;
+            }
+
+            this.color = color;
+            rect = transform.gameObject.GetComponent<RectTransform>();
+            image = rect.GetComponent<Image>();
+            image.color = color;
+        }
+
+
+        public bool Update()
+        {
+            
+            Vector3 targetScreenPosition = uiCam.WorldToScreenPoint(targetPosition);
+            
+            float borderSize = 40f;
+            bool isOffScreen = targetScreenPosition.x <= borderSize || targetScreenPosition.x >= Screen.width - borderSize || targetScreenPosition.y <= borderSize || targetScreenPosition.y >= Screen.height - borderSize;
+
+            Vector3 captureTargetScreenPosition = targetScreenPosition;
+            
+            if (isOffScreen)
+            {
+                
+                RotatePointerTowardsTarget();
+                
+                image.sprite = spritePointer;
+
+                captureTargetScreenPosition = new Vector3(Mathf.Clamp(captureTargetScreenPosition.x, borderSize, Screen.width - borderSize), Mathf.Clamp(captureTargetScreenPosition.y, borderSize, Screen.height - borderSize));
+
+                captureTargetScreenPosition = uiCam.ScreenToWorldPoint(captureTargetScreenPosition);
+                rect.position = captureTargetScreenPosition;
+                rect.localPosition = new Vector3(rect.localPosition.x, rect.localPosition.y, 0f);
+            }
+            else
+            {
+                image.sprite = spriteInstruct;
+                captureTargetScreenPosition = uiCam.ScreenToWorldPoint(captureTargetScreenPosition);
+                rect.position = captureTargetScreenPosition;
+                rect.localPosition = new Vector3(rect.localPosition.x, rect.localPosition.y, 0f);
+                rect.localEulerAngles = new Vector3(0, 0, 0);
+            }
+
+            
+
+            return CalculateDistance();
+        }
+
+
+        private void RotatePointerTowardsTarget()
+        {
+            Vector3 toPosition = targetPosition;
+            Vector3 fromPosition = uiCam.transform.position;
+            fromPosition.z = 0;
+
+            Vector3 dir = (toPosition - fromPosition).normalized;
+            float eulerZ = Utils.GetAngleFromVector(dir);
+            rect.localEulerAngles = new Vector3(0, 0, eulerZ);
+
+        }
+
+
+        public bool CalculateDistance()
+        {
+
+            float distance = Vector3.Distance(targetPosition, player.position);
+            if (distance > 2f && distance <= 4f)
+            {
+                color.a = 0f;
+                image.color = color;
+
+            }
+            else if(distance > 4f)
+            {
+                color.a = 1f;
+                image.color = color;
+            }
+            else if(distance <= 2f && QuestFinishTurnToAnswerPoinetr())
+            {
+
+                Destroy(transform.gameObject);
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private bool QuestFinishTurnToAnswerPoinetr()
+        {
+
+            if (stat == 1)
+            {
+                return true;
+            }
+
+            stat = 1;
+            Vector3 newTargetPosition = new Vector3(UnityEngine.Random.Range(-18f , 18f) , UnityEngine.Random.Range(-10f , 10f));
+            targetPosition = newTargetPosition;
+            spriteInstruct = spriteAnswer;
+
+            return false;
+        }
+
     }
 }
